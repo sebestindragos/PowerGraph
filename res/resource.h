65c7075ce6bef7d7107823562e24094f65910fab
sebestindragos <sebestin.dragos@gmail.com>
2013-10-24 13:16:19 -0700

    Modified toolbar and icon resources.

--- res/resource.h
+++ res/resource.h
@@ -1,21 +1,21 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by PowerGraph.RC
+// Used by PowerGraph.rc
 //
-
-
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-//#define IDR_POWERGRAPHTYPE	129
-#define IDD_POWERGRAPH_FORM	129
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDD_POWERGRAPH_FORM             129
+#define ID_FILE_ADD_NODE                32775
+#define ID_FILE_DELETE_NODE             32776
+#define ID_FILE_LINK_NODE               32777
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	201
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32775
+#define _APS_NEXT_RESOURCE_VALUE        207
+#define _APS_NEXT_COMMAND_VALUE         32784
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
